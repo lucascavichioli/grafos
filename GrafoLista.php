df9e5be4bef84024d14626bfc0e04e9786c2d81d
marlon101tkm <50722508+marlon101tkm@users.noreply.github.com>
2019-08-26 11:34:57 -0300

    Ultimas Atualizações

--- GrafoLista.php
+++ GrafoLista.php
@@ -3,36 +3,62 @@ require_once "Grafos.php";
 
 class GrafoLista extends Grafos {
     
+    public $grafo = [];
+    public $nomeVertices=[];
+
+
     function __construct(bool $ehdirecionado, bool $ehponderado){
         parent::__construct(bool $ehdirecionado, bool $ehponderado);
     }
  
     public function inserirVertice(string $label) : bool {
-        
+        $this->$grafo[] = [];
+        $nomeVertices[]=$vertice;
         
     }
     
     public function labelVertice(int $indice) : string{
           
-        
+        return $this->$nomeVertices[$indice];
     }
     
     public function imprimeGrafo() : void {
-        
+
     }
     
     public function inserirAresta(int $origem, int $destino, int $peso = 1) : bool {
-        
+        if($this->$ehponderado){
+            $this->grafo[$origem][] = [$nomeVertices[$destino],$peso];
+        }else{
+            $this->grafo[$origem][] = $nomeVertices[$destino];
+        }
         
     }
     
     public function existeAresta(int $origem, int $destino) : bool {
         
+        if($this->$ehponderado){
+            for($coluna = 0; $coluna < count($this->grafo[$origem]); $linha++){
+                if($this->grafo[$origem][$coluna][0] == $nomeVertices[$destino]){
+                    return true;
+                }
+            }
+            return false;
+        }else{
+            for($coluna = 0; $coluna < count($this->grafo[$origem]); $linha++){
+                if($this->grafo[$origem][$coluna] == $nomeVertices[$destino]){
+                    return true;
+                }
+            }
+            return false;
+        }
+
         
     }
     
     public function retornarVizinhos(int $vertice) : array { //vector<int>
         
+        return $this->$grafo[$vertice]  
     }
     
 }
--- GrafoMatriz.php
+++ GrafoMatriz.php
@@ -3,48 +3,84 @@ require_once "Grafos.php";
 
 class GrafoMatriz extends Grafos{
     
-    public $grafo = array();
-    public $v = 0;
+    public $grafo = [];
+    public $nomeVertices=[];
     
     function __construct(bool $ehdirecionado, bool $ehponderado){
         parent::__construct($ehdirecionado, $ehponderado);
     }
     
     public function inserirVertice(string $vertice) : bool {
-        for($linha = 1; $linha < count($linha); $linha++){
-            for($coluna = 1; $coluna <= count($this->grafo); $coluna++){
-                $grafo[$vertice][$vertice] = 0;
-            }
-        }
-        return true; 
         
-        //$this->grafo += array($vertice => array($vertice => 1));
-        return true;
+        $this->$grafo[] = [];
+        for($linha = 0; $linha < count($this->grafo); $linha++){
+             $this->$grafo[$linha] = array_pad($this->$grafo[$linha],count($this->grafo),0);
+        }
+        $nomeVertices[]=$vertice;
+        
+        
+        // for($linha = 0; $linha < count($linha); $linha++){
+        //     for($coluna = 0; $coluna <= count($this->grafo); $coluna++){
+        //         $grafo[$vertice][$vertice] = 0;
+        //     }
+        // }
+        // return true; 
+        
+        // //$this->grafo += array($vertice => array($vertice => 1));
+        // return true;
     }
     
     public function labelVertice(int $indice) : string{
            
+        return $this->$nomeVertices[$indice];
         
     }
     
     public function imprimeGrafo() {
-        print "<pre>";
-        print_r($this->grafo);
-        print "</pre>";
+
+        for ($row = 0; $row < count($grafo) ; $row++) {
+          echo " ".$row.": ";
+          for ($col = 0; $col < count($grafo[$row]); $col++) {      
+            echo "".$grafo[$row][$col]."";
+          }
+            echo "<br>";
+         }
+
+
+        // print "<pre>";
+        // print_r($this->grafo);
+        // print "</pre>";
     }
     
     public function inserirAresta(int $origem, int $destino, int $peso = 1) : bool {
-        
+        if($this->$ehdirecionado){
+            $this->grafo[$origem][$destino] = $peso;
+            return true; 
+        } else {
+            $this->grafo[$origem][$destino] = $peso;
+            $this->grafo[$destino][$origem] = $peso;
+            return true; 
+        }
         
     }
     
     public function existeAresta(int $origem, int $destino) : bool {
         
+        return $this->grafo[$origem][$destino] != 0;  
         
     }
     
     public function retornarVizinhos(int $vertice) : array { //vector<int>
-        
+        $lista =[];
+        for($coluna = 0; $coluna < count($this->grafo[$vertice]); $linha++){
+           if($this->grafo[$vertice][$coluna] != 0){
+                $lista[] = $this->$nomeVertices[$coluna];
+           }
+       }
+
+
+        return $lista;
+
     }
     
 }
